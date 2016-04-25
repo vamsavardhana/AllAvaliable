c588cd5133ca40b7d040e00e9eb67e89ea301417
anna <vamsavardhana.vijay@gmail.com>
2016-04-24 19:46:06 -0700

    Correction: SMS sent without going to SMS application

--- app/src/main/java/edu/cmu/nsompura/allavailable/ui/sendsms.java
+++ app/src/main/java/edu/cmu/nsompura/allavailable/ui/sendsms.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,9 +38,8 @@ public class sendsms extends Activity{
                 String smsText = et2.getText().toString();
 
                 Uri uri = Uri.parse("smsto:" + smsNumber);
-                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
-                intent.putExtra("sms_body", smsText);
-                startActivity(intent);
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(smsNumber, null, smsText, null, null);
             }});
 
     }
