47b8230eda34aa670a8e1985b5ac535c36fd53a2
anna <vamsavardhana.vijay@gmail.com>
2016-04-24 20:28:55 -0700

    Messages can now be sent to multiple participants separated by comma, no exception handling required if inputting character text in place of numbers as sms manager autocorrects and uses only the number to send sms to

--- app/src/main/java/edu/cmu/nsompura/allavailable/ui/sendsms.java
+++ app/src/main/java/edu/cmu/nsompura/allavailable/ui/sendsms.java
@@ -5,10 +5,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 import edu.cmu.nsompura.allavailable.R;
 
@@ -28,18 +34,44 @@ public class sendsms extends Activity{
         btn16=(Button)findViewById(R.id.btn16);
         et1=(EditText)findViewById(R.id.et1);
         et2=(EditText)findViewById(R.id.et2);
-        btn16.setOnClickListener(new Button.OnClickListener(){
 
+
+        //Return logic
+        btn15.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                Intent myintent = new Intent("android.intent.action.SELECTION");
+                startActivity(myintent);
+            }});
+
+
+
+
+        //Send SMS logic
+        btn16.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View arg0) {
                 // TODO Auto-generated method stub
-
                 String smsNumber = et1.getText().toString();
                 String smsText = et2.getText().toString();
-
-                Uri uri = Uri.parse("smsto:" + smsNumber);
+                //create array list
+                List<String> numbers = new ArrayList<String>();
+                //split smsNumber by comma
+                StringTokenizer st1 = new StringTokenizer(smsNumber, ",");
+                //assign arraylist elements to sms numbers
+                while (st1.hasMoreTokens()) {
+                    numbers.add(st1.nextToken());
+                }
+                //start SMS Manager
                 SmsManager smsManager = SmsManager.getDefault();
-                smsManager.sendTextMessage(smsNumber, null, smsText, null, null);
+                Log.i("size", numbers.size()+"");
+                //send SMS to all the participants in the meeting
+                for(int i=0;i<numbers.size();i++) {
+                    smsManager.sendTextMessage(numbers.get(i), null, smsText, null, null);
+                    Log.i("mesage sent to",numbers.get(i)+"");
+                }
+                //Notification that message has been sent
+                Toast.makeText(getApplicationContext(), "The message has been sent", Toast.LENGTH_LONG).show();
             }});
 
     }
